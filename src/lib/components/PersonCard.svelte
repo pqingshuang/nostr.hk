563e057e89139d5209619d68dd768c6afb63353e
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-04-18 22:54:31 +0200

    Basic fix to /join page

--- src/lib/components/PersonCard.svelte
+++ src/lib/components/PersonCard.svelte
@@ -27,8 +27,8 @@
 <a
     href={url}
     target="_blank"
-    class="personCard flex flex-row gap-4 items-center border border-purple-800 p-2 rounded-md overflow-hidden shadow-lg"
+    class="personCard flex flex-row gap-4 items-center border border-purple-800 p-2 rounded-md overflow-hidden shadow-lg hover:shadow-xl"
 >
     <img src={profileImage} alt="profile" class="bg-purple-600 rounded-full w-10 h-10 m-0" />
-    <span class="m-0 border-0">{name}</span>
+    <span class="m-0 border-0 font-medium">{name}</span>
 </a>
--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -1,6 +1,33 @@
 <script lang="ts">
     import { _, isLoading } from 'svelte-i18n';
     import Members from '$lib/components/Members.svelte';
+    import ndk from '$lib/stores/ndk';
+    import type { NDKFilter } from '@nostr-dev-kit/ndk';
+
+    // TODO: REFACTOR TO ONE PLACE
+    const fetchEventsAndUserIds: Promise<App.EventsAndUserIds> = new Promise<App.EventsAndUserIds>(
+        async (resolve, reject) => {
+            try {
+                // This is G's original note
+                const rootEventId =
+                    '8d61f3346a9875bfd135a17793e13b1235843abac2ba86529b58294dadabc23a';
+
+                const rootEventFilter: NDKFilter = { ids: [rootEventId] };
+                const rootEvent = await $ndk.fetchEvent(rootEventFilter);
+
+                const threadFilter: NDKFilter = { kinds: [1], '#e': [rootEventId] };
+                const threadEvents = Array.from(await $ndk.fetchEvents(threadFilter));
+
+                const fullThread = [rootEvent, ...threadEvents];
+                const userIds = fullThread.map((item) => item.pubkey);
+                const uniqUserIds = [...new Set(userIds)];
+
+                resolve({ events: fullThread, userIds: uniqUserIds });
+            } catch (error) {
+                reject(error);
+            }
+        }
+    );
 </script>
 
 {#if !$isLoading}
@@ -26,9 +53,14 @@
     </div>
     <div class="bg-purple-900 py-8 mt-16">
         <div class="proseContainer !mt-0">
-            <h2 class="!mt-0">{$_('nostrHkMembers.title')}</h2>
+            <h2 class="!mt-0">
+                {#await fetchEventsAndUserIds then eventsAndIds}
+                    {eventsAndIds.userIds.length}
+                {/await}
+                {$_('nostrHkMembers.title')}
+            </h2>
         </div>
 
-        <Members {userIds} />
+        <Members eventsAndIds={fetchEventsAndUserIds} />
     </div>
 {/if}
